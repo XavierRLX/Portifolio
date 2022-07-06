049d811aa397f9ffc20ad785036e38f92e8b7129
XavierRLX <renanlima2000.aer@gmail.com>
2022-07-05 23:16:42 -0300

    stily 3.0

---
+++ js/digitando.js
@@ -0,0 +1,15 @@
+
+
+setTimeout(() => {
+    preEscricao.style.display = 'inline'
+}, 1800);
+
+setTimeout(() => {
+    descricao.style.display = 'inline'
+}, 2800);
+
+
+
+     let hDois = document.getElementById('hmdois');
+     let descricao = document.getElementById('descrição');
+     let preEscricao = document.getElementById('preEscrição');
