13261b56c785a86332affe297b8f67c2cfdcb5dc
TemperoDoDivino <91702228+XavierRLX@users.noreply.github.com>
2022-08-04 23:07:12 -0300

    add sobre e contato

---
+++ js/copiarEcolar.js
@@ -0,0 +1,10 @@
+function copiarEm (){
+    var copiarN = document.getElementById("copiarNumb").value;
+    
+    navigator.clipboard.writeText(copiarN);
+}
+
+function copiarNumb (){
+    var copiarE = document.getElementById("copiarEmail");
+
+}
