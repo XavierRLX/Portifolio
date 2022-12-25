7922099462afce26a7d507136be4999c45f1a244
TemperoDoDivino <91702228+XavierRLX@users.noreply.github.com>
2022-12-25 15:08:19 -0300

    Ondas e Slide corridos

--- js2/script.js
+++ js2/script.js
@@ -24,6 +24,38 @@ var swiper = new Swiper(".slide-content", {
         },
         950: {
             slidesPerView: 3,
+        }
+    },
+  });
+
+  ///////////////////////
+
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
         },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
     },
   });
