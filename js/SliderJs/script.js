cf1b267b3d4b128f4ec6990d9e0778aa489276a9
Renan Xavier <renanlima2000.aer@gmail.com>
2023-02-10 11:07:29 -0300

    Edições
    Edição nas pastas.
    Editado a cor de fundo do "sobre".
    Slide alterado no modo mobile.

---
+++ js/SliderJs/script.js
@@ -0,0 +1,32 @@
+
+  ///////////////////////
+
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        900: {
+            slidesPerView: 2,
+        },
+        1100: {
+            slidesPerView: 3,
+        }
+    },
+  });
